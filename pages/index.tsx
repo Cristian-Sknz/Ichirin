d6375e654ebcb727a4e8d1c3abd9bb467dec1b96
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2021-10-14 13:36:56 -0300

    [fix] Corrigido import incorreto de `MangaData` Por algum motivo a IDE não fez o seu trabalho de verificar os imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import Head from 'next/head';
 import FeatureCarrousel from '../components/layouts/Home/FeatureCarrousel';
 import FeaturedCompleted from '../components/layouts/Home/FeaturedCompleted';
 import LastMangaUpdates from '../components/layouts/Home/LastMangaUpdates';
-import { MangaData } from '../components/layouts/Home/MangaList/MangaList';
 
 import Navbar from '../components/template/Navbar';
 import Topbar from '../components/template/Topbar';
 import Footer from '../components/template/Footer';
+import MangaData from '../lib/types';
 
 export default function Home({mangas} : InferGetStaticPropsType<typeof getStaticProps>) {
   return (
