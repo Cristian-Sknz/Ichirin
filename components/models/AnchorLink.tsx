f9b811c6320b3721a3e3eae382d3cab60e35f37f
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2021-10-23 17:02:36 -0300

    [feature] adicionado classname no anchorlink

--- components/models/AnchorLink.tsx
+++ components/models/AnchorLink.tsx
@@ -1,13 +1,14 @@
 import Link, { LinkProps } from 'next/link'
 
 interface AnchorLink extends LinkProps {
+    className?: string;
     children?: JSX.Element | JSX.Element[] | string;
 }
 
 export default function AnchorLink(props : AnchorLink) {
     return (
         <Link {...props}>
-            <a>{props.children}</a>
+            <a className={props.className}>{props.children}</a>
         </Link>
     );
     
